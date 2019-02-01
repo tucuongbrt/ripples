f0e557a8bcb398a7cec3511a4f08a8b606cb8564
Renato Campos <renatoineeve@gmail.com>
2019-02-01 15:00:02 +0000

    ripples-web/src/App: Add router.

--- ripples-web/src/App.js
+++ ripples-web/src/App.js
@@ -1,13 +1,21 @@
-import React, { Component } from "react";
-import Ripples from "./Ripples";
+import React, { Component } from 'react';
+import Ripples from './Ripples';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SoiRisk from './SoiRisk';
+import Home from './Home';
 
 class App extends Component {
   render() {
     return (
-        <Ripples></Ripples>
-        
-    );
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/ripples' exact={true} component={Ripples}/>
+          <Route path='/soirisk' exact={true} component={SoiRisk}/>
+        </Switch>
+      </Router>
+    )
   }
 }
- 
+
 export default App;
