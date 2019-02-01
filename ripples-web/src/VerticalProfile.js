cc3801ae2bd4313d16e32c86f6befa78c15f840f
Renato Campos <renatoineeve@gmail.com>
2019-02-01 15:03:57 +0000

    ripples-web/src/VerticalProfile: Component to render a profile marker.

---
+++ ripples-web/src/VerticalProfile.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {Marker, Popup} from 'react-leaflet'
+import { getSystemPosition } from './PositionUtils';
+import LinePlot from './LinePlot';
+import { SensorIcon } from './icons/Icons';
+
+export default class VerticalProfile extends Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <Marker position={getSystemPosition(this.props.data)} icon={new SensorIcon()}>
+                <Popup minWidth={500} maxWidth={1024}>
+                    <LinePlot data={this.props.data}></LinePlot>
+                </Popup>
+            </Marker>
+        )
+    }
+}
