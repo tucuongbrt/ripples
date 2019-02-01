4b81d815227234e126969cb9c7fa13979ea09ffd
Renato Campos <renatoineeve@gmail.com>
2019-02-01 14:49:54 +0000

    ripples-web/src/DateUtils: Add methods to handle dates

---
+++ ripples-web/src/DateUtils.js
@@ -0,0 +1,21 @@
+
+import moment from 'moment'
+
+export function timestampSecToReadableDate(timestamp){
+    return formatDate(new Date(timestamp*1000))
+}
+
+export function timestampMsToReadableDate(timestamp){
+    return formatDate(new Date(timestamp))
+}
+function formatDate(date){
+    return moment(date).format('MMMM Do YYYY, h:mm:ss a')
+}
+
+export function secondsToTime(secondsInput){
+    let absSeconds = Math.abs(secondsInput);
+    let hours = Math.floor(absSeconds/3600);
+    let minutes = Math.floor(absSeconds/60);
+    let seconds = absSeconds % 60;
+    return `${secondsInput < 0 ? '-' : ''}${hours}h ${minutes}m ${seconds}s`
+}
