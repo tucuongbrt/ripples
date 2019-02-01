8b2fd1480d1a7dfd7aa975d1897c068704ed8634
Renato Campos <renatoineeve@gmail.com>
2019-02-01 14:54:02 +0000

    ripples-web/src/LinePlot: Add class responsible for drawing line charts

---
+++ ripples-web/src/LinePlot.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {Line} from 'react-chartjs-2'
+import { timestampMsToReadableDate } from './DateUtils';
+
+/**
+ * Responsible for drawing line charts
+ */
+export default class LinePlot extends Component {
+    constructor(props){
+        super(props)
+        let x = [];
+        let y = [];
+        let humanReadableTime = timestampMsToReadableDate(props.data.timestamp);
+        props.data.samples.forEach(point => {
+            x.push((point[0]/10).toFixed(1))
+            y.push(point[1])
+        })
+
+        this.state = {
+            data: {
+                labels: x,
+                datasets: [{ 
+                    data: y,
+                    label: props.data.type,
+                    borderColor: "#ff0000",
+                    fill: false
+                  }]
+                },
+            options: {
+                title: {
+                    display: true,
+                    text: `${props.data.type} data collected at ${humanReadableTime}`
+                },
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: `${props.data.type}`
+                        }
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'depth'
+                        }
+                    }]
+                },
+                maintainAspectRatio: false
+            }
+        }
+    }
+    render() {
+        
+        return (
+            <Line data={this.state.data} width={500} height={500} options={this.state.options}>
+            </Line>
+        )
+    }
+}
