08a375f90a69797396f08d7a46cb1b4f781fd740
Renato Campos <renatoineeve@gmail.com>
2019-02-01 15:02:16 +0000

    ripples-web/src/Vehicle: Component to render a vehicle.

---
+++ ripples-web/src/Vehicle.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import {Marker, Popup} from 'react-leaflet'
+import {getSystemPosition} from './PositionUtils'
+import {timestampSecToReadableDate} from './DateUtils'
+import {AuvIcon} from './icons/Icons'
+
+export default class Vehicle extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            lastState: props.lastState,
+            name: props.name,
+            plan: null
+        };
+        this.setPlan.bind(this);
+    }
+
+    setPlan(newPlan){
+        this.setState({plan: newPlan});
+    }
+
+    render(){
+        let vehicle = this.state;
+        console.log('draw vehicle called')
+        return (
+            <Marker position={getSystemPosition(vehicle.lastState)} icon={new AuvIcon()}>
+                <Popup>
+                    <h3>{vehicle.name}</h3>
+                    <ul>
+                    <li>Lat: {vehicle.lastState.latitude.toFixed(5)}</li>
+                    <li>Lng: {vehicle.lastState.longitude.toFixed(5)}</li>
+                    <li>Fuel: {vehicle.lastState.fuel}</li>
+                    <li>Heading: {vehicle.lastState.heading.toFixed(3)}</li>
+                    <li>Date: {timestampSecToReadableDate(vehicle.lastState.timestamp)}</li>
+                    </ul>
+                </Popup>
+            </Marker>        
+        );
+        
+    }
+
+
+}
