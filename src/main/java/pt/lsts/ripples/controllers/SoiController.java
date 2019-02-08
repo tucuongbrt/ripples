bc81093c61cd4c892c5b9e0fa443824d905d700e
Renato Campos <renatoineeve@gmail.com>
2019-02-08 16:18:01 +0000

    pt.lsts.ripples.controller.SoiController:Add route to predict collisions."

--- src/main/java/pt/lsts/ripples/controllers/SoiController.java
+++ src/main/java/pt/lsts/ripples/controllers/SoiController.java
@@ -2,6 +2,7 @@ package pt.lsts.ripples.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,15 +10,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import pt.lsts.ripples.domain.assets.Asset;
+import pt.lsts.ripples.domain.soi.VehicleRiskAnalysis;
 import pt.lsts.ripples.domain.soi.VerticalProfileData;
 import pt.lsts.ripples.repo.AssetsRepository;
 import pt.lsts.ripples.repo.VertProfilesRepo;
+import pt.lsts.ripples.services.CollisionForecastService;
 
 @RestController
 public class SoiController {
 
 	@Autowired
 	AssetsRepository repo;
+	
+	@Autowired
+	CollisionForecastService collisionService;
 
 	@Autowired
 	VertProfilesRepo vertProfiles;
@@ -34,6 +40,12 @@ public class SoiController {
 		ArrayList<VerticalProfileData> profs = new ArrayList<>();
 		vertProfiles.findAll().forEach(profs::add);
 		return profs;
+	}
+	
+	@RequestMapping(path = { "/soi/risk", "/soi/risk/" }, method = RequestMethod.GET)
+	public ConcurrentHashMap<String, VehicleRiskAnalysis> riskAnalysis() {
+		ConcurrentHashMap<String, VehicleRiskAnalysis> vehiclesRisk = collisionService.updateCollisions();
+		return vehiclesRisk;
 	}
 	
 
