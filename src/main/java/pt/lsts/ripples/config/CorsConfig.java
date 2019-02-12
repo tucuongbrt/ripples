64b317d16eb44a255732a82e05ca133fe05788d3
Renato Campos <renatoineeve@gmail.com>
2019-02-12 12:22:49 +0000

    pt.lsts.ripples.config: Organize Imports.

--- src/main/java/pt/lsts/ripples/config/CorsConfig.java
+++ src/main/java/pt/lsts/ripples/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package pt.lsts.ripples.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
