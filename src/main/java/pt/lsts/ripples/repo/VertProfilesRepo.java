b6383f8d66186ba9f19c10327eb11f7dd3655f87
ineeve <renatoineeve@gmail.com>
2019-02-19 18:02:20 +0000

    pt.lsts.ripples.repo.VertProfilesRepo: Add missing @Repository annotation.

--- src/main/java/pt/lsts/ripples/repo/VertProfilesRepo.java
+++ src/main/java/pt/lsts/ripples/repo/VertProfilesRepo.java
@@ -4,8 +4,9 @@ import java.util.Date;
 
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.stereotype.Repository;
 import pt.lsts.ripples.domain.soi.VerticalProfileData;
-
+@Repository
 public interface VertProfilesRepo extends CrudRepository<VerticalProfileData, Long> {
 	Iterable<VerticalProfileData> findByTimestampAfter(Date since);
 	Iterable<VerticalProfileData> findBySystem(String sourceName);
