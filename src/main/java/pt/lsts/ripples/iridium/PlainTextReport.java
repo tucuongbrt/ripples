7877d369beba98159bed29b5cefa2afdab276028
ineeve <renatoineeve@gmail.com>
2019-02-19 18:02:54 +0000

    pt.lsts.ripples: Delete old iridium utils and create a PlainTextReport class.

---
+++ src/main/java/pt/lsts/ripples/iridium/PlainTextReport.java
@@ -0,0 +1,88 @@
+package pt.lsts.ripples.iridium;
+
+import pt.lsts.imc.*;
+import pt.lsts.ripples.domain.assets.AssetPosition;
+import pt.lsts.ripples.util.DateUtil;
+
+import java.util.Collection;
+import java.util.Vector;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PlainTextReport extends IridiumMessage {
+
+    private static Pattern p = Pattern.compile("\\((.)\\) \\((.*)\\) (.*) / (.*), (.*) / .*");
+
+    String report;
+    AssetPosition position;
+
+    public PlainTextReport() {
+        super(-1);
+    }
+
+    @Override
+    public int serializeFields(IMCOutputStream out) throws Exception {
+        out.writePlaintext(report);
+        out.close();
+        return report.getBytes("ISO-8859-1").length + 2;
+    }
+
+    @Override
+    public int deserializeFields(IMCInputStream in) throws Exception {
+        byte[] data = new byte[200];
+        int len = in.read(data);
+        report = new String(data, "ISO-8859-1");
+        parse();
+        return len + 2;
+    }
+
+    @Override
+    public Collection<IMCMessage> asImc() {
+        Vector<IMCMessage> msgs = new Vector<>();
+        msgs.add(new TextMessage("iridium", report));
+        return msgs;
+    }
+
+    @Override
+    public String toString() {
+        return "Report: " + report + "\n";
+    }
+
+    public AssetPosition getAssetPosition(){
+        return position;
+    }
+
+    private void parse() {
+        Matcher matcher = p.matcher(report);
+        if (!matcher.matches()) {
+            return;
+        }
+        String vehicle = matcher.group(2);
+        String timeOfDay = matcher.group(3);
+        String latMins = matcher.group(4);
+        String lonMins = matcher.group(5);
+        int source = IMCDefinition.getInstance().getResolver().resolve(vehicle);
+        if (source == -1) {
+            return;
+        }
+        String latParts[] = latMins.split(" ");
+        String lonParts[] = lonMins.split(" ");
+        double lat = getCoords(latParts);
+        double lon = getCoords(lonParts);
+
+        position = new AssetPosition();
+        position.setTimestamp(DateUtil.parseTimeString(timeOfDay));
+        position.setLat(lat);
+        position.setLon(lon);
+        position.setName(vehicle);
+        position.setImcId(source);
+    }
+
+    private double getCoords(String[] coordParts) {
+        double coord = Double.parseDouble(coordParts[0]);
+        coord += (coord > 0) ? Double.parseDouble(coordParts[1]) / 60.0 : -Double.parseDouble(coordParts[1]) / 60.0;
+        return coord;
+    }
+}
+
+
