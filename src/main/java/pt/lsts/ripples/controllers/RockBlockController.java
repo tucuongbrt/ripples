e8ff14ec229e338336d799f4d0cf8d1b59a149a3
ineeve <renatoineeve@gmail.com>
2019-02-25 16:27:23 +0000

    pt.lsts.ripples.controllers.RockBlockController: Redirect messages sent to /api/v1/iridium to rockblock.

--- src/main/java/pt/lsts/ripples/controllers/RockBlockController.java
+++ src/main/java/pt/lsts/ripples/controllers/RockBlockController.java
@@ -4,10 +4,11 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
-import java.util.logging.Logger;
 
 import javax.xml.bind.annotation.adapters.HexBinaryAdapter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import pt.lsts.ripples.domain.iridium.Rock7Message;
 import pt.lsts.ripples.iridium.IridiumMessage;
+import pt.lsts.ripples.iridium.RockBlockIridiumSender;
 import pt.lsts.ripples.repo.Rock7Repository;
 import pt.lsts.ripples.services.MessageProcessor;
 
@@ -28,7 +30,9 @@ public class RockBlockController {
 
 	private static final HexBinaryAdapter hexAdapter = new HexBinaryAdapter();
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
-	
+
+	private static Logger logger = LoggerFactory.getLogger(RockBlockController.class);
+
 	static {
 		dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 
@@ -39,6 +43,9 @@ public class RockBlockController {
 
 	@Autowired
 	private MessageProcessor msgProcessor;
+
+	@Autowired
+	private RockBlockIridiumSender rockBlockService;
 
 	@GetMapping(path = "/api/v1/iridium")
 	public List<Rock7Message> pollMessages() {
@@ -52,6 +59,11 @@ public class RockBlockController {
         return repo.findPlainTextSince(d);
     }
 
+	/**
+	 * Gateway that saves a messages and redirects it to the rockBlock HTTP API.
+	 * @param body
+	 * @return
+	 */
 	@SuppressWarnings("rawtypes")
 	@PostMapping(path = {"/api/v1/iridium", "/api/v1/irsim"}, consumes = "application/hub")
 	public ResponseEntity sendMessage(@RequestBody String body) {
@@ -60,12 +72,13 @@ public class RockBlockController {
 			msg = IridiumMessage.deserialize(hexAdapter.unmarshal(body));
 		}
 		catch (Exception e) {
-		    return new ResponseEntity<String>(e.getClass().getSimpleName() + " while sending Iridium message",
+			e.printStackTrace();
+		    return new ResponseEntity<String>(e.getClass().getSimpleName() + ": deserialize Iridium message error",
 			HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		int dst = msg.getDestination();
 		int src = msg.getSource();
-
+		logger.info("Message dst: " + dst + "; msg src: "  + src);
 		Rock7Message m = new Rock7Message();
 		m.setType(msg.getMessageType());
 		m.setDestination(dst);
@@ -78,6 +91,13 @@ public class RockBlockController {
 		repo.save(m);
 
 		msgProcessor.process(msg);
+		try {
+			rockBlockService.sendMessage(msg); // redirect message to rockBlock
+		} catch(Exception e){
+			e.printStackTrace();
+			return new ResponseEntity<String>(e.getClass().getSimpleName() + ": redirect Iridium message error",
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		return new ResponseEntity<String>("Message posted to Ripples", HttpStatus.OK);
 	} 	
 
@@ -95,7 +115,7 @@ public class RockBlockController {
 		try {
 			timestamp = dateFormat.parse(transmit_time);
 		} catch (Exception e) {
-			Logger.getLogger(getClass().getName()).warning("Unable to parse date");
+			logger.warn("Unable to parse date");
 			return new ResponseEntity<String>("Unable to parse the date.", HttpStatus.BAD_REQUEST);
 		}
 
@@ -116,7 +136,7 @@ public class RockBlockController {
 			m.setPlainText(msg.getMessageType() == -1);
 		} catch (Exception e) {
 		    e.printStackTrace();
-			Logger.getLogger(getClass().getName()).warning("Unable to parse message data:" + e.getMessage());
+			logger.warn("Unable to parse message data:" + e.getMessage());
 			return new ResponseEntity<String>(
 					"Unable to parse message data:" + e.getMessage(),
 					HttpStatus.INTERNAL_SERVER_ERROR);
