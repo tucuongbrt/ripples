dba67d78950e35524671289286192d484ab33460
ineeve <renatoineeve@gmail.com>
2019-02-25 16:30:49 +0000

    pt.lsts.ripples.tests.RockBlockTests: Replace tests on /api/v1/iridum route.

--- src/main/java/pt/lsts/ripples/tests/RockBlockTest.java
+++ src/main/java/pt/lsts/ripples/tests/RockBlockTest.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class RockBlockTest {
 
-    public static final String TIME = "19-02-19 15:00:00";
+    public static final String TIME = "25-02-19 14:00:00";
 
     public static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://localhost:9090/")
@@ -22,7 +22,7 @@ public class RockBlockTest {
     private static final String UNKNOWN_PLAIN_TEXT = "556E6B6E6F776E20666F726D6174206D657373616765";
     private static final String PLAIN_TEXT_MSG = "28522920286c6175762d78706c6f72652d31292031343a34393a3536202f20343120332e3339313930312c202d382034312e393835323231202f20663a2d3120763a32363220633a2d31202f20733a204d202f20703a2d31";
 
-
+    private static final String MT_SOI_COMMAND = "63001e00da075403ad4f645c010100005303000005005203e11f2442511c0bc1c04f645c3c005203311a2442ac0c0bc1b352645c3c0052038214244208fd0ac1a655645c3c005203d20e244265ed0ac19a58645c3c005203f90e244296cf0ac16559645c3c000000";
     private static final String IMC1 = "1e00ffffda07540317c66a5c02050000ffff0000";
     private static final String IMC2 = "1e00ffffda07540326c66a5c020100005303286402005203e639244235320bc14dc66a5c3c005203ae3824429f360bc129c76a5c3c000000";
     private static final String IMC3 = "1e00ffffd5071e00494e464f3a2046696e697368656420706c616e20657865637574696f6e2e";
@@ -55,11 +55,7 @@ public class RockBlockTest {
     }
 
     private static void testIridiumWrapper(){
-        testIridium(PLAIN_TEXT_MSG);
-        testIridium(IMC1);
-        testIridium(IMC2);
-        testIridium(IMC3);
-        testIridium(IMC4);
+        testIridium(MT_SOI_COMMAND);
     }
 
     private static void testRock7Wrapper(){
@@ -73,7 +69,7 @@ public class RockBlockTest {
     }
 
     public static void main(String args[]){
-        testIridiumWrapper();
         testRock7Wrapper();
+        testIridiumWrapper();
     }
 }
