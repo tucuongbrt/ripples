e1d445265334121225d0e38d0fd979fdd436979d
ineeve <renatoineeve@gmail.com>
2019-02-26 13:40:47 +0000

    pt.lsts.ripples.tests.RockBlockTest: Fix XP1 IMEI value.

--- src/main/java/pt/lsts/ripples/tests/RockBlockTest.java
+++ src/main/java/pt/lsts/ripples/tests/RockBlockTest.java
@@ -21,7 +21,7 @@ public class RockBlockTest {
 
     private static final String UNKNOWN_PLAIN_TEXT = "556E6B6E6F776E20666F726D6174206D657373616765";
     private static final String PLAIN_TEXT_MSG = "28522920286c6175762d78706c6f72652d31292031343a34393a3536202f20343120332e3339313930312c202d382034312e393835323231202f20663a2d3120763a32363220633a2d31202f20733a204d202f20703a2d31";
-
+    private static final String XP1_IMEI = "300234061464450";
 
     private static final String IMC1 = "1e00ffffda07540317c66a5c02050000ffff0000";
     private static final String IMC2 = "1e00ffffda07540326c66a5c020100005303286402005203e639244235320bc14dc66a5c3c005203ae3824429f360bc129c76a5c3c000000";
@@ -35,7 +35,7 @@ public class RockBlockTest {
     }
 
     public static void testRock7(String data){
-        Call<ResponseBody> call = service.postRock7("+351964085112", TIME, data);
+        Call<ResponseBody> call = service.postRock7(XP1_IMEI, TIME, data);
         executeCall(call);
     }
 
