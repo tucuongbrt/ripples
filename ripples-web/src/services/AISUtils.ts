bcb6b471d38413a5015d245af457d64fbd41a478
Renato Campos <renatoineeve@gmail.com>
2019-04-18 14:46:32 +0100

    Display only recent aisShips

--- ripples-web/src/services/AISUtils.ts
+++ ripples-web/src/services/AISUtils.ts
@@ -1,7 +1,13 @@
 import IAisShip from "../model/IAisShip";
 
+const fourHoursInMs = 14400000; 
+
+function isRecent(ship: IAisShip): boolean {
+  return ship.updated_at > (Date.now() - fourHoursInMs);
+}
+
 export function fetchAisData() {
   return fetch(`${process.env.REACT_APP_API_BASE_URL}/ais`)
     .then(response => response.json())
-    .then((ships: IAisShip[]) => ships) 
+    .then((ships: IAisShip[]) => ships.filter(ship => isRecent(ship))); 
 };
