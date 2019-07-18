78b904aee53913541b5963f112ad9b693c29e570
ineeve <renatoineeve@gmail.com>
2019-07-18 12:04:35 +0100

    SoiRisk: Improve next communication and fuel display

--- ripples-web/src/scenes/SoiRisk/SoiRisk.tsx
+++ ripples-web/src/scenes/SoiRisk/SoiRisk.tsx
@@ -99,6 +99,11 @@ class SoiRisk extends Component<PropsType & GeolocatedProps, StateType> {
     })
   }
 
+  /**
+   * Calculate the index of the next waypoint. Returns -1 if there's no waypoint in the future.
+   * @param vehicle
+   * @param plan
+   */
   public getNextWaypointIdx(vehicle: IAsset, plan: IPlan) {
     const waypoints = plan.waypoints
     if (waypoints.length === 0) {
@@ -109,6 +114,9 @@ class SoiRisk extends Component<PropsType & GeolocatedProps, StateType> {
     const minInterval = Math.min(...timeIntervals)
     const minIntervalIdx = timeIntervals.findIndex(d => d === minInterval)
     const minIntervalWP = waypoints[minIntervalIdx]
+    if (minIntervalWP.timestamp === 0) {
+      return -1
+    }
     // distance in meters
     const distanceToMinInterval = distanceInMetersBetweenCoords(minIntervalWP, vehicle.lastState)
     // if distance is less than 100m, let's consider that the vehicle already arrived to that waypoint
@@ -151,6 +159,9 @@ class SoiRisk extends Component<PropsType & GeolocatedProps, StateType> {
   }
 
   public buildFuel(fuel: number) {
+    if (fuel === -1) {
+      return <td className={'bg-red'}>N/D</td>
+    }
     return <td className={fuel > 0.5 ? 'bg-green' : fuel > 0.1 ? 'bg-orange' : 'bg-red'}>{fuel.toFixed(2)}</td>
   }
 
