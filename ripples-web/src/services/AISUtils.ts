a5272fadd931305422cd926e35ca75b68839983d
ineeve <renatoineeve@gmail.com>
2019-07-24 17:11:45 +0100

    Handle AIS websockets endpoint

--- ripples-web/src/services/AISUtils.ts
+++ ripples-web/src/services/AISUtils.ts
@@ -2,19 +2,26 @@ import IAisShip from '../model/IAisShip'
 import { calculateShipLocation, estimatePositionsAtDeltaTime } from './PositionUtils'
 
 const oneHourInMs = 3600000
+const deltaHours = 12
 
-function isRecent(ship: IAisShip): boolean {
-  return ship.timestamp > Date.now() - oneHourInMs
-}
+export default class AISService {
+  public async fetchAisData(): Promise<IAisShip[]> {
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/ais`)
+    let ships = await response.json()
+    ships = ships.map((s: IAisShip) => this.convertAISToRipples(s))
+    return ships.filter((s: IAisShip) => this.isRecent(s))
+  }
 
-export async function fetchAisData(): Promise<IAisShip[]> {
-  const deltaHours = 12
-  const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/ais`)
-  const ships = await response.json()
-  ships.forEach((s: IAisShip) => {
+  /**
+   * Convert AIS payload that is fetched from the server into a usable format
+   */
+  public convertAISToRipples(s: IAisShip): IAisShip {
     s.timestamp = new Date(s.timestamp).getTime()
     s.awareness = estimatePositionsAtDeltaTime(s, deltaHours)
     s.location = calculateShipLocation(s)
-  })
-  return ships.filter((s: IAisShip) => isRecent(s))
+    return s
+  }
+  private isRecent(ship: IAisShip): boolean {
+    return ship.timestamp > Date.now() - oneHourInMs
+  }
 }
