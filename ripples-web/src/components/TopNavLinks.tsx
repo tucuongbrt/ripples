22196c20c657f0ec553fefb30d5dda5ecf677703
Sandro Campos <up201605947@fe.up.pt>
2019-07-24 17:52:06 +0100

    Added TopNavbar icons

--- ripples-web/src/components/TopNavLinks.tsx
+++ ripples-web/src/components/TopNavLinks.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Nav from 'reactstrap/lib/Nav'
+import logo from '../assets/logo.png'
 
 /**
  * Should be reused by the other implementations of the navbar
@@ -8,15 +9,15 @@ import Nav from 'reactstrap/lib/Nav'
 export default class TopNavLinks extends Component {
   public render() {
     return (
-      <Nav className="mr-auto" navbar={true}>
+      <Nav id="nav" className="mr-auto" navbar={true}>
         <Link className="navbar-link" to="/">
-          Ripples
+          <img id="logo" src={logo} alt="logo" />
         </Link>
         <Link className="navbar-link" to="/soirisk">
-          Soi Risk Analysis
+          <i title="Soi Risk Analysis" className="fas fa-exclamation-triangle fa-lg" />
         </Link>
         <Link className="navbar-link" to="/messages/text">
-          Text Messages
+          <i title="Text Messages" className="fas fa-envelope-open-text fa-lg" />
         </Link>
       </Nav>
     )
