9df2c2654fdeba48bc3f57a6f60235811c5158b7
ineeve <renatoineeve@gmail.com>
2019-07-30 15:50:08 +0100

    Set MyAnnotation to use sequencial ids

--- src/main/java/pt/lsts/ripples/domain/logbook/MyAnnotation.java
+++ src/main/java/pt/lsts/ripples/domain/logbook/MyAnnotation.java
@@ -4,13 +4,14 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class MyAnnotation {
 
   @Id
-  @GeneratedValue
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   private String content;
