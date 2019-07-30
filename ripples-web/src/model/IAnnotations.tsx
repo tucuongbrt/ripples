949eda33605827aec27470a8bbb9e1e3597b84ed
ineeve <renatoineeve@gmail.com>
2019-07-30 11:25:07 +0100

    Add IAnnotation

--- ripples-web/src/model/IAnnotations.tsx
+++ ripples-web/src/model/IAnnotations.tsx
@@ -1,10 +1,26 @@
-export default interface IAnnotation {
+import ILatLng from './ILatLng'
+
+export default interface IAnnotation extends INewAnnotation {
   id: number
-  content: string
   username: string
   date: Date
 }
 
+export interface INewAnnotation {
+  content: string
+  location: ILatLng
+}
+
 export class Annotation implements IAnnotation {
-  constructor(public id: number, public content: string, public username: string, public date: Date) {}
+  constructor(
+    public id: number,
+    public content: string,
+    public username: string,
+    public date: Date,
+    public location: ILatLng
+  ) {}
+}
+
+export class NewAnnotation implements INewAnnotation {
+  constructor(public content: string, public location: ILatLng) {}
 }
