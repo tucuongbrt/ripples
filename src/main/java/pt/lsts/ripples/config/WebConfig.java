6f77c80485772dea70ae491476080290c2354a84
Sandro Campos <up201605947@fe.up.pt>
2020-08-19 16:08:26 +0100

    Feature: Config Coral resources to be served

---
+++ src/main/java/pt/lsts/ripples/config/WebConfig.java
@@ -0,0 +1,31 @@
+package pt.lsts.ripples.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*")
+                .allowCredentials(true).maxAge(MAX_AGE_SECS);
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/coral/**").addResourceLocations("classpath:/coral/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/coral").setViewName("forward:/coral/index.html");
+        registry.addViewController("/coral/").setViewName("forward:/coral/index.html");
+    }
+}
