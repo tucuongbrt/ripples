4973f325c11b6afc054b37af935a95e8d8c80cc7
Jose Pinto <zepinto@gmail.com>
2021-03-16 22:27:26 +0000

    Fixed config.sh fore new repo structure.

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd src/main/resources
 git clone https://github.com/LSTS/ripples-private
-cp -v ripples-private/config/* .
+cp -v ripples-private/config/* src/main/resources
+mv -v src/main/resources/application_development.yml src/main/resources/application.yml
 rm -rf ripples-private
